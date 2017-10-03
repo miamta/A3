841cfcc330cf2ba348f050c596c16b24c0a355e7
Miguel Amores Tamarit <miamta@floridauniversitaria.es>
2017-10-03 13:08:15 +0200

    Acividad 3

---
+++ A3.js
@@ -0,0 +1,13 @@
+// variablres
+  var numero=80 ;
+  var numero2=prompt('Ingrese un numero:','');
+
+
+// if para informar sobre si se acierta el numero
+if (numero>numero2) {
+  alert("Te quedaste corto")
+}else if (numero<numero2) {
+  alert("Te pasaste")
+}else if (numero=numero2) {
+  alert("ACERTASTE")
+}
